d63ba899f4854ce60377d3805d8bcc1edb439054
yuanzichao <yuanzichao90@126.com>
2016-01-21 10:15:17 +0800

    Product class

--- src/com/xxx/bean/Product.java
+++ src/com/xxx/bean/Product.java
@@ -6,5 +6,50 @@ package com.xxx.bean;
  * @date 2016年1月20日 下午5:42:23
  */
 public class Product {
+	
+	private long productId;
+	private String definition;
+	
+	
+	/**
+	 * @param productId
+	 */
+	public Product(long productId) {
+		super();
+		this.productId = productId;
+	}
 
+	/**
+	 * @param productId
+	 * @param definition
+	 */
+	public Product(long productId, String definition) {
+		super();
+		this.productId = productId;
+		this.definition = definition;
+	}
+
+	
+	/**
+	 * @return the productId
+	 */
+	public long getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @return the definition
+	 */
+	public String getDefinition() {
+		return definition;
+	}
+
+	/**
+	 * @param definition the definition to set
+	 */
+	public void setDefinition(String definition) {
+		this.definition = definition;
+	}
+	
+	
 }
