479cda5a648c49717e7d1b163a5b66824a47a655
yuanzichao <yuanzichao90@126.com>
2016-01-21 10:46:59 +0800

    Simulator class

---
+++ src/com/xxx/simulator/Simulator.java
@@ -0,0 +1,23 @@
+package com.xxx.simulator;
+
+/**
+ * @author yuan
+ *
+ * @date 2016年1月21日 上午10:46:11
+ */
+public class Simulator {
+
+	/**
+	 * Name: main
+	 * Description: 
+	 * @param args
+	 *
+	 * @author yuan
+	 * @date 2016年1月21日 上午10:46:11
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
